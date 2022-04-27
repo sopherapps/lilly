331f24feb5c40f094007e71062d66d0dad5f9d5f
Martin <sales@sopherapps.com>
2022-04-27 16:06:34 +0300

    Add tests for routing

---
+++ test/test_routing.py
@@ -0,0 +1,32 @@
+"""
+Module containing tests for routing
+"""
+from unittest import main, TestCase
+from unittest.mock import patch, Mock
+
+from lilly.actions import Action
+from lilly.routing import RouteSet
+
+
+class TestRouteSet(TestCase):
+    """Test for the RouteSet class"""
+
+    @patch.object(Action, "run")
+    @patch.object(Action, "__init__", return_value=None)
+    def test_do(self, mock_action_init: Mock, mock_action_run: Mock):
+        """the _do method makes a call to the Action"""
+        expected = "foo bar"
+        mock_action_run.return_value = expected
+        args = ["some", "stuff"]
+        kwargs = {"other": "stuff"}
+
+        route_set = RouteSet()
+        response = route_set._do(Action, *args, **kwargs)
+
+        mock_action_init.assert_called_with(*args, **kwargs)
+        mock_action_run.assert_called_once()
+        self.assertEqual(expected, response)
+
+
+if __name__ == '__main__':
+    main()
